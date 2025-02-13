fb96d7bf43d45b0ce3690c78fe0450523e80d9bc
Ashish Kalbande <154236313+Ashishkalbande11@users.noreply.github.com>
2025-02-13 09:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const textEl = document.getElementById("text");
+const speedEl = document.getElementById("speed");
+ 
+const text = textEl.innerText;
+ 
+let idx = 1;
+ 
+const NUM = 500;
+let time = NUM / speedEl.value;
+ 
+writeText();
+ 
+function writeText() {
+  textEl.innerHTML = text.slice(0, idx);
+ 
+  idx++;
+ 
+  if (idx > text.length) {
+    idx = 1;
+  }
+ 
+  setTimeout(writeText, time);
+}
+ 
+speedEl.addEventListener("input", (e) => {
+  time = NUM / e.target.value;
+});;
